39409e769d7365a10870561e8081be05aead8c07
esrefacil <48657873+esrefacil@users.noreply.github.com>
2022-01-10 14:43:12 -0800

    listener.pl
    Just a Listener

---
+++ listener.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use IO::Socket::INET;
+
+# Autoflush stdout
+$| = 1;
+
+my $listener = new IO::Socket::INET (Listen    => 1,
+                                     LocalAddr => '0.0.0.0',
+                                     LocalPort => '1234',
+                                     Proto     => 'tcp');
+
+my $client_socket = $listener->accept();
+
+while (1) {
+  my $data = "";
+  $client_socket->recv($data, 1024);
+  print "$data";
+  if ($data eq "") {
+    last;
+  }
+}
+shutdown($client_socket, 1);
