e30cd97c4d89265f0695b493336ddb9078beb5c9
esrefacil <48657873+esrefacil@users.noreply.github.com>
2022-01-10 14:50:13 -0800

    Create 7070.pl

---
+++ 7070.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use IO::Socket;
+my $sock = new IO::Socket::INET ( 
+   LocalPort => '7070', 
+   Proto => 'tcp', 
+   Listen => 1, 
+   Reuse => 1, 
+);
+
+die "Could not create socket: $!\n" unless $sock;
+
+my $new_sock = $sock->accept();
+while(<$new_sock>) { print $_; }
+close($sock);
